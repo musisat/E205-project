f60e0b5f9492a316201efbacc006f13a87e1ef12
musisat <kkimberling@hmc.edu>
2015-11-15 19:58:30 -0800

    Made global variables

---
+++ placeElectrodes.m
@@ -0,0 +1,8 @@
+function placeElectrodes( numElectrodes )
+%NUMELECTRODES will place the given number of electrodes evenly around the
+% grid boundary and keep track of which two electrodes are injecting the
+% current signal ("drive electrodes").
+
+
+end
+
