7e3b3814bb59af553fd13d6fb7153c1b42c50ec9
musisat <kkimberling@hmc.edu>
2015-11-21 11:05:18 -0800

    Never mind, that was stupid.

--- createConductivityGrid.m
+++ createConductivityGrid.m
@@ -14,36 +14,14 @@ global conductivityGrid radius
 % I don't know if complex impedance matters.
 % Cycle over entire matrix insert impedance values inside the boundary.
 % Cells outside the boundary should have infinite impedance.
-% impedance = 0.025;
-% cellLength = 30e-6; % For now assume 30 microns.
-% resistivity = impedance*cellLength;
-% conductivity = 1/resistivity;
-% 
-% for i=1:2*radius
-%     for j=1:2*radius
-%         if sqrt((i-radius-1)^2+(j-radius-1)^2) < radius
-%             conductivityGrid(i,j) = conductivity;
-%         else
-%             conductivityGrid(i,j) = 0; % We may need to adjust the boundary.
-%         end
-%     end
-% end
-% imagesc(conductivityGrid)
-
-% Non-homogeneous domain! Determine conductivities probabilistically. They
-% should all be pretty close to each other.
-meanImpedance = 0.025;
+impedance = 0.025;
 cellLength = 30e-6; % For now assume 30 microns.
+resistivity = impedance*cellLength;
+conductivity = 1/resistivity;
 
 for i=1:2*radius
     for j=1:2*radius
         if sqrt((i-radius-1)^2+(j-radius-1)^2) < radius
-            impedance = normrnd(meanImpedance,0.01);
-            if impedance<0
-                impedance = meanImpedance;
-            end
-            resistivity = impedance*cellLength;
-            conductivity = 1/resistivity;
             conductivityGrid(i,j) = conductivity;
         else
             conductivityGrid(i,j) = 0; % We may need to adjust the boundary.
